91727eab3557a2b6c0d9c5bf1cfd10e710c9c2de
Ryan Mirch <rwmirch@gmail.com>
2022-08-22 01:55:36 -0400

    fixed warnings

--- 2d_demo.gd
+++ 2d_demo.gd
@@ -9,10 +9,6 @@ var damage_number_2d_pool:Array[DamageNumber2D] = []
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 	
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("Left Click") or event.is_action_pressed("Right Click"):
--- Damage_Number_2D.gd
+++ Damage_Number_2D.gd
@@ -11,9 +11,6 @@ extends Node2D
 func _ready() -> void:
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 	
 func set_value(value:String) -> void:
 	label.text = value
