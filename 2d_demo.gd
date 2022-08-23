dd22614238aa183f3109b0dc95e5b24d84b52f26
Ryan Mirch <rwmirch@gmail.com>
2022-08-22 21:53:12 -0400

    Polished demo

--- 2d_demo.gd
+++ 2d_demo.gd
@@ -1,29 +1,48 @@
-extends Node2D
+extends Control
 
 @onready var spawn_point = %SpawnPoint
 @onready var damage_number_2d_template = load("res://Damage_Number_2D.tscn")
+@onready var spread_value:Label = %"Spread Value"
+@onready var height_value:Label = %"Height Value"
 
 var damage_number_2d_pool:Array[DamageNumber2D] = []
+var rmb_held = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+func _process(delta: float) -> void:
+	if rmb_held:
+		spawn_damage_number(randf_range(0,10))
+
 	
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("Left Click") or event.is_action_pressed("Right Click"):
+	if event.is_action_pressed("Left Click"):
 		spawn_damage_number(randf_range(0,10))
+		
+	if event.is_action_pressed("Right Click"):
+		rmb_held = true
+		
+	if event.is_action_released("Right Click"):
+		rmb_held = false
+		
 	
 func spawn_damage_number(value:float):
-	var damage_number = get_damage_number()
-	add_child(damage_number, true)	
-	damage_number.set_value(str(round(value)))
-	damage_number.position = spawn_point.position
+	var damage_number = get_damage_number()	
+	var val = str(round(value))
+	var pos = spawn_point.position
+	var height = height_value.text.to_float()
+	var spread = spread_value.text.to_float()
+	add_child(damage_number, true)
+	damage_number.set_values_and_animate(val, pos, height, spread)
 
 func get_damage_number() -> DamageNumber2D:
+	# get a damage number from the pool
 	if damage_number_2d_pool.size() > 0:
 		return damage_number_2d_pool.pop_front()
-		
+	
+	# create a new damage number if the pool is empty
 	else:
 		var new_damage_number = damage_number_2d_template.instantiate()
 		new_damage_number.tree_exiting.connect(
@@ -32,3 +51,11 @@ func get_damage_number() -> DamageNumber2D:
 		
 	
 		
+
+
+func _on_spread_value_changed(value: float) -> void:
+	spread_value.text = str(value)
+
+
+func _on_height_value_changed(value: float) -> void:
+	height_value.text = str(value)
--- Damage_Number_2D.gd
+++ Damage_Number_2D.gd
@@ -5,19 +5,18 @@ extends Node2D
 @onready var label_container:Node2D = %LabelContainer
 @onready var ap:AnimationPlayer = %AnimationPlayer
 
-@export var up_distance = 120
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 	
-func set_value(value:String) -> void:
+func set_values_and_animate(value:String, start_pos:Vector2, height:float, spread:float) -> void:
 	label.text = value
 	ap.play("Rise and Fade")
+	
 	var tween = get_tree().create_tween()
-	var start_pos = Vector2(randf_range(-10,10),randf_range(-5,5))
-	var end_pos = Vector2(randf_range(-30,30),-up_distance)
+	var end_pos = Vector2(randf_range(-spread,spread),-height) + start_pos
 	var tween_length = ap.get_animation("Rise and Fade").length
 	
 	tween.tween_property(label_container,"position",end_pos,tween_length).from(start_pos)
